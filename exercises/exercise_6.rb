e8ebdfea7a5925f442e778dc69839a1eca9563c5
Chad Bruhaug <churfzilla@gmail.com>
2016-10-11 14:02:03 -0700

    Finished ex 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,25 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Chad", last_name: "Bruhaug", hourly_rate: 50)
+
+@store1.employees.create(first_name: "Taylor", last_name: "Acoin", hourly_rate: 52)
+
+@store1.employees.create(first_name: "Mitch", last_name: "Oniel", hourly_rate: 50)
+
+@store1.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Jorah", last_name: "Mormont", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Sandor", last_name: "Clegane", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Arya", last_name: "Stark", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Catelynn", last_name: "Tully", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Jamie", last_name: "Lanister", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Walder", last_name: "Frey", hourly_rate: 60)
+
